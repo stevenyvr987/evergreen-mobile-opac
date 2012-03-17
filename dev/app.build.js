a8f9f7308ada5b40887e298f4dfdd43f70b2e04a
schan <schan@sitka.bclibraries.ca>
2012-03-17 20:46:05 +0000

    Define how r.js is to build an optimized version of javascript files This is a preliminary version that allows us to build an optimized version of the first page.  It does not include CSS files nor images, etc.  It does not build optimized versions of any other pages.  Also, we cannot define a coffeescript version and compile it to javascript, because of a shortcoming in how r.js parses the compiled javascript version.

---
+++ dev/app.build.js
@@ -0,0 +1,20 @@
+
+({
+  baseUrl: '../js-compile',
+  dir: '../js-build',
+  modules: [
+    {
+      name: 'mobile_opac'
+    }
+  ],
+  paths: {
+    jquery: 'empty:',
+    jqm: 'empty:',
+    json2: 'empty:',
+    jsd: 'lib/jsdeferred',
+    md5: 'lib/md5',
+    jqm_sd: 'lib/jquery.mobile.simpledialog2',
+    fmall: 'dojo/fieldmapper/fmall',
+    fmd: 'eg/fm_datatypes'
+  }
+})
